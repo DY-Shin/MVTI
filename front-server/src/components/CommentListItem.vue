f6fb0a483f29a449e4126391854575c37fae6782
Jungeun Park <pyooooong@gmail.com>
2022-11-19 22:53:25 +0900

    comment edit

--- front-server/src/components/CommentListItem.vue
+++ front-server/src/components/CommentListItem.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
     <li>
-      <span>{{ comment.username }} - {{ comment.content }}</span>
-      <button type="submit" class="btn btn-primary" @click="deleteComment">삭제</button>
-      <button type="submit" class="btn btn-primary" @click="updateComment">수정</button>
-      
+      <!-- 댓글 수정 중 -->
+      <div v-if="isEditing">
+        <input 
+        type="text" 
+        id="comment"
+        v-model.trim="commentContent">
+        <button type="submit" class="btn btn-primary" @click="updateComment">수정</button>
+      </div>
+      <div v-else>
+
+        <span>{{ comment.username }} - {{ comment.content }}</span>
+        <button type="submit" class="btn btn-primary" @click="deleteComment">X</button>
+        <button type="submit" class="btn btn-primary" @click="SwitchIsEditing">수정</button>
+      </div>
     </li>
   </div>
 </template>
@@ -18,7 +28,23 @@ export default {
   props: {
     comment: Object,
   },
+  data() {
+    return {
+      isEditing: false,
+      commentContent: this.comment.content,
+      commentUser: this.comment.username,
+      currentUser: this.$store.state.username
+    }
+  },
   methods: {
+    SwitchIsEditing() {
+      if (this.currentUser === this.commentUser) {
+        this.isEditing = !this.isEditing
+      } else {
+        alert('본인만 수정 가능합니다!')
+      }
+      // this.isEditing = !this.isEditing
+    },
     deleteComment() {
       const movie_id = this.$route.params.id
       const comment_id = this.comment.id
@@ -36,11 +62,33 @@ export default {
       })
       .catch((err) => {
         console.log(err)
-        // alert('본인만 삭제가능합니다!')
+        alert('본인만 삭제 가능합니다!')
       })
     },
     updateComment() {
-      
+      const movie_id = this.$route.params.id
+      const comment_id = this.comment.id
+      const content = this.commentContent
+
+      axios({
+        method: 'put',
+        url: `${API_URL}/api/v1/movies/comments/${comment_id}/`,
+        data: {
+          content
+        },
+        headers: {
+          Authorization: `Token ${this.$store.state.token}`
+        }
+      })
+      .then((res) => {
+        console.log(res)
+        this.isEditing = !this.isEditing
+        this.$store.commit('GET_COMMENTS', movie_id)
+      })
+      .catch((err) => {
+        console.log(err)
+        // alert('본인만 수정가능합니다!')
+      })
     }
   }
   // computed: {
