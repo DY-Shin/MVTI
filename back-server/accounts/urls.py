bf4dd7e166d9df176ea78bf47fd92d3854ce506b
Jungeun Park <pyooooong@gmail.com>
2022-11-24 09:13:17 +0900

    수정

--- back-server/accounts/urls.py
+++ back-server/accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('<int:user_pk>/', views.userdetail),
     path('<int:user_pk>/mvti/<int:mvti_pk>/', views.usermvti),
-    path('accounts/<str:username>/comments', views.user_comments),
+    # path('accounts/<str:username>/comments', views.user_comments),
 ]
