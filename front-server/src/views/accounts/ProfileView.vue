04d5e3784ceb5f0238aac261bf0fc0bfbcef4da1
Jungeun Park <pyooooong@gmail.com>
2022-11-24 15:59:18 +0900

    modify

--- front-server/src/views/accounts/ProfileView.vue
+++ front-server/src/views/accounts/ProfileView.vue
@@ -17,9 +17,13 @@
       <h5>아직 MVTI 검사를 하지 않으셨군요!</h5>
       <router-link :to="{ name: 'survey1' }"><h5>MVTI 검사하고 나에게 맞는 영화 추천받기</h5></router-link>
     </div>
-    <div class="fw-bold text-center text-white my-5">
-        <h4>코멘트 {{ usercomments.length }}개</h4>
-        <!-- <h4>찜한 영화 {{ profile.pick_movies.length }}개</h4> -->
+    <div v-if="usercomments">
+      <div class="fw-bold text-center text-white my-5">
+          <h4>코멘트 {{ usercomments.length }}개</h4>
+      </div>
+    </div>
+    <div v-if="likemovie">
+      <div class="fw-bold text-center text-white my-5"></div>
         <h4>좋아요한 영화 {{ likemovie.length }}개</h4>
     </div>
     <hr>
